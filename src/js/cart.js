7faac74426e389abfe6e14ffd61526442929c448
sosath <sosalijeronthomas@gmail.com>
2026-06-05 14:04:53 -0400

    Team Activity 03: Dynamic Header and Footer - implement AJAX-loaded templates

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,6 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, loadHeaderFooter } from './utils.mjs';
+
+await loadHeaderFooter();
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
